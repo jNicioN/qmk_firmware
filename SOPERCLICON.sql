145ccf15df7d9dbbc3bf87d5c736738d1893bced
Erick Alanis <erick.alanis@banregio.com>
2019-01-18 10:57:49 -0600

    @ Erick 2018112842000075 Sps Sincronizacion

--- SOPERCLICON.sql
+++ SOPERCLICON.sql
@@ -18,6 +18,12 @@ as
 /***************************************************************************
 ** Descripción:	 Consulta Persona y Cliente								****
 ****************************************************************************
+** Modificó:	Josue Leal Ramirez										****
+** Fecha:		03/Diciembre/2018										****
+** Help Desk:	1160018													****
+** Descripción:	Se agrega consulta C6 que retorna las personas de grupo	****
+**				IDE														****
+****************************************************************************
 ** Modifico:	Felipe Castillo Rendon									****
 ** Fecha:		16-08-2017												****
 ** Help:		989254  												****
@@ -34,7 +40,11 @@ as
 ****************************************************************************/
 													/* Declaración de variables */
 declare	@Tip_ConTip	char(1),
-		@Tip_ConCon	char(1)
+		@Tip_ConCon	char(1),
+		@Str_Vacio  char(1)
+		
+										/* Asignación de constantes */
+select	@Str_Vacio	= ''				/* String vacío */		
 
 select	@Tip_ConTip	= substring(@Tip_Consul, 1, 1),
 		@Tip_ConCon	= substring(@Tip_Consul, 2, 1)
@@ -76,6 +86,27 @@ if @Tip_ConTip = 'C' begin							/* 'C':  Consulta */
 		left 	join CLADICIO noholdlock on Adi_NumPer = Per_Numero 
 		where  	PerPersoID  = @PerPersoID
 	
+	end else	if @Tip_ConCon = '6' begin						/* Consulta unificada por Cli_Numero o Per_Numero*/	
+	
+		select	ClClientID,  PerPersoID ,  Adi_Client as Cli_Numero , Per_Numero
+			from CLCLIUNI Cli noholdlock
+				inner join CLCLIUNI Gpo noholdlock on Cli.Clu_Grupo = Gpo.Clu_Grupo
+				inner join CLADICIO noholdlock on Gpo.Clu_Client = Adi_Client 
+				inner join SOPERSON noholdlock on Adi_NumPer = Per_Numero
+			where @Cli_Numero != @Str_Vacio and Cli.Clu_Client = @Cli_Numero  
+		union  
+		select	 AdiGpo.ClClientID,  PerGpo.PerPersoID ,  AdiGpo.Adi_Client as Cli_Numero , PerGpo.Per_Numero
+			from SOPERSON Per
+				inner join CLADICIO Adi noholdlock on  Per.Per_Numero = Adi.Adi_NumPer
+				inner join CLCLIUNI Cli noholdlock on  Adi.Adi_Client = Cli.Clu_Grupo
+				inner join CLADICIO AdiGpo noholdlock on Cli.Clu_Client = AdiGpo.Adi_Client
+				inner join SOPERSON PerGpo noholdlock on AdiGpo.Adi_NumPer =  PerGpo.Per_Numero
+			where @Per_Numero != @Str_Vacio and Per.Per_Numero = @Per_Numero
+		union
+		select	ClClientID,  PerPersoID ,  Adi_Client as Cli_Numero , Per_Numero
+			from SOPERSON  
+				 left join CLADICIO on  Per_Numero  =  Adi_NumPer 
+			where @Per_Numero != @Str_Vacio and Per_Numero = @Per_Numero	
 	end
 	
 end
