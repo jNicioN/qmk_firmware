beb98fd74655d91587dc99462ea487176cd65e9e
Edwin Dennis <edwin.santiago@banregio.com>
2019-06-20 10:53:04 -0500

    1212881 - Se agrega tipo de consulta a SOUSUREGCON
    Se agrega consulta para traer solo las zonas activas

--- SOUSUREGCON.sql
+++ SOUSUREGCON.sql
@@ -17,11 +17,12 @@ as
 /******************************************************************/
 /* DESCRIPCION: Usuario por regiones							  */
 /******************************************************************/
-/* Modifica:	Jaret Guanajuato Ruvalcaba						****
-** Fecha:		06/05/2019										****
-** Help:		1058568 										****
-** Modifica:	Se agrega Reg_SegNum a L3						****
-*******************************************************************/
+/* Modifica:	Edwin Dennis Santiago							****
+** Fecha:		19/06/2019										****
+** Help:		1212881  										****
+** Modifica:	Se agrega consulta L7 para solo traer regiones 	****
+**				activas											***/
+/******************************************************************/
 /* Modifica:	Jorge A. Garcia Leal							****
 ** Fecha:		20/07/2017										****
 ** Help:		985322 											****
@@ -40,12 +41,13 @@ as
 ** Fecha:		03/10/2016										****
 ** Help:		903360 											****
 ********************************************************************/
-/* Declaracion de Variables */
-declare	@Tip_ConTip	char(1),
-		@Tip_ConCon	char(1),
-		@Int_Index int
 
-/* Asignacion de Constantes */
+/* Declaracion de Variables */
+declare	@Tip_ConTip	char(1),		/* Tipo de Consulta*/
+		@Tip_ConCon	char(1),		/* Numero de consulta*/
+		@Int_Index int				/* Entero Indice*/
+
+/* declaracion de Constantes */
 declare	@Str_Vacio	char(1),
 		@Str_Activo	char(1),
 		@Str_Cero	char(1),
@@ -56,28 +58,25 @@ declare	@Str_Vacio	char(1),
 		@Str_Cuatro	char(1),
 		@Str_Cinco	char(1),
 		@Str_Seis	char(1),
-		@Str_Coma	char(1),
-		@Int_Uno	int,
-		@Tip_Lista	char(1)
-
-select	@Str_Vacio	= '',			/* Caracter Vacio			*/
-		@Str_Activo	= 'A',
-		@Str_Cero	= '0',
-		@Int_Cero	= 0,
-		@Str_Uno	= '1',
-		@Str_Dos	= '2',
-		@Str_Tres	= '3',
-		@Str_Cuatro	= '4',
-		@Str_Cinco	= '5',
-		@Str_Seis	= '6',
-		@Str_Coma   = ',',
-		@Int_Uno	= 1,
-		@Tip_Lista  = 'L'
+		@Str_Siete	char(1)
+		
+/* Asignacion de constantes*/
+select	@Str_Vacio	= '',			/* Caracter Vacio*/
+		@Str_Activo	= 'A',			/* Estatus A*/
+		@Str_Cero	= '0',			/* Caracter Cero*/
+		@Int_Cero	= 0,			/* Entero Cero*/
+		@Str_Uno	= '1',			/* Caracter Uno*/
+		@Str_Dos	= '2',			/* Caracter Dos*/
+		@Str_Tres	= '3',			/* Caracter Tres*/
+		@Str_Cuatro	= '4',			/* Caracter Cuatro*/
+		@Str_Cinco	= '5',			/* Caracter Cinco*/
+		@Str_Seis	= '6',			/* Caracter Seis*/
+		@Str_Siete	= '7'			/* Caracter Siete*/
 
 select	@Tip_ConTip	= substring(@Tip_Consul, 1, 1),
 		@Tip_ConCon	= substring(@Tip_Consul, 2, 1)
 
-if @Tip_ConTip	= @Tip_Lista begin			/* 'L': Listas */
+if @Tip_ConTip	= 'L' begin					/* 'L': Listas */
 	if @Tip_ConCon	= @Str_Uno begin		/* L1 Usuarios por Region */
 		select	Usr_Region,	Reg_Descri,	Usr_Usuari,	Usu_Clave,	Usu_Nombre
 			from SOUSUREG noholdlock
@@ -95,8 +94,7 @@ if @Tip_ConTip	= @Tip_Lista begin			/* 'L': Listas */
 			order by Reg_Descri
 	end
 	if @Tip_ConCon	= @Str_Tres begin /* L3 Regiones por Numero de Usuario */
-		select	Usr_Region,	Reg_Descri,	Usr_Usuari,	Usu_Clave,	Usu_Nombre,
-				Reg_SegNum
+		select	Usr_Region,	Reg_Descri,	Usr_Usuari,	Usu_Clave,	Usu_Nombre
 			from SOUSUREG noholdlock
 			inner join SOUSUARI noholdlock on Usu_Numero = Usr_Usuari
 			inner join SOREGION noholdlock on Reg_Numero = Usr_Region
@@ -121,13 +119,13 @@ if @Tip_ConTip	= @Tip_Lista begin			/* 'L': Listas */
 		create table #Regiones_Tmp(id int)
 				
 		/* Se separan los IDs de Zonas*/
-		select @Int_Index = charindex(@Str_Coma, @Usr_MulReg)
+		select @Int_Index = charindex(',', @Usr_MulReg)
 		while @Int_Index > @Int_Cero begin
 			insert into #Regiones_Tmp
-				values (CONVERT(INT,left(@Usr_MulReg, @Int_Index-@Int_Uno)))
+				values (CONVERT(INT,left(@Usr_MulReg, @Int_Index-1)))
 		
-			set @Usr_MulReg	= substring(@Usr_MulReg, @Int_Index+@Int_Uno, datalength(@Usr_MulReg) - @Int_Index)
-			select @Int_Index = charindex(@Str_Coma, @Usr_MulReg)
+			set @Usr_MulReg	= substring(@Usr_MulReg, @Int_Index+1, datalength(@Usr_MulReg) - @Int_Index)
+			select @Int_Index = charindex(',', @Usr_MulReg)
 		end
 		
 		if (datalength(@Usr_MulReg) > @Int_Cero) begin
@@ -159,5 +157,14 @@ if @Tip_ConTip	= @Tip_Lista begin			/* 'L': Listas */
 			  and	Upe_Perfil	= @Usr_Perfil
 			  and	Usr_Usuari	= @Usr_Usuari
 			order by Usu_Nombre
+	end
+	if @Tip_ConCon	= @Str_Siete begin /* L7 Regiones activas por numero de usuario */
+		select	Usr_Region,	Reg_Descri,	Usr_Usuari,	Usu_Clave,	Usu_Nombre
+			from SOUSUREG noholdlock
+			inner join SOUSUARI noholdlock on Usu_Numero = Usr_Usuari
+			inner join SOREGION noholdlock on Reg_Numero = Usr_Region
+			where	Usr_Usuari = @Usr_Usuari 
+			  and  Reg_Status = @Str_Activo
+			order by Reg_Descri
 	end
 end
