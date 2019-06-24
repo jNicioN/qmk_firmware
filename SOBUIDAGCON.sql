d48795646bb1171c1e854b86f0b0dca777065ee8
Pedro <pedro.delosreyes@banregio.com>
2019-06-24 16:55:44 -0500

    procedimiento para obtener el id del agente

---
+++ SOBUIDAGCON.sql
@@ -0,0 +1,27 @@
+create procedure SOBUIDAGCON (
+	@Bia_IdAsesor int,
+	@NumTransac	char(10),
+	@Transaccio	char(3),
+	@Usuario	char(6),
+	@FechaSis	smalldatetime,
+	@SucOrigen	char(3),
+	@SucDestino	char(3),
+	@Modulo		char(2))
+as
+/****************************************************************************
+*** REFERENCIAS: 														****
+****************************************************************************
+** Creo:	Pedro de los Reyes											****
+** Fecha:		17/Marzo/2019											****
+****************************************************************************/
+
+declare	@Agente 	int, /* Declaracion de Variables */
+     	@Id_Agente 	int /* Declaracion de Variables */
+     	
+select @Id_Agente=SoAgenteID from SOREAGAS where SoUsuariID = @Bia_IdAsesor
+
+if @Id_Agente is null begin
+	select @Id_Agente=0
+end
+
+select @Id_Agente as SoAgenteID
