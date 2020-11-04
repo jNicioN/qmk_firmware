2b39e6a4202255e7693bbd956db530428dde8aaf
raul.muniz <raul.muniz@banregio.com>
2020-11-04 17:45:20 -0600

    SPs de generalidades

---
+++ SORIBGENALT.sql
@@ -0,0 +1,53 @@
+create procedure SORIBGENALT (
+   @Rig_Numero int,
+   @Rig_NumRib int,
+   @Rig_ActCre varchar(10),
+   @Rig_TiDeGo int,
+   @Rig_DepGob int,
+   @Rig_TieExp int,
+   @Rig_Export int,
+   @Rig_TiGeDi int,
+   @Rig_GeCoMa int,
+   @Rig_GenCob int,
+   @Rig_GeNoCo int,
+   @Rig_Activo bit,
+
+   @NumTransac char(10),
+   @Transaccio char(3),
+   @Usuario char(6),
+   @FechaSis smalldatetime,
+   @SucOrigen char(3),
+   @SucDestino char(3),
+   @Modulo char(2)) 
+ as 
+/********************************************************************/
+/* DESCRIPCION: Alta de registros de Generalidades asociados a RIB	*/
+/********************************************************************/
+/* Creo:		Raul Muniz											*/
+/* Fecha:		03/11/2020											*/
+/* Help:		1433413												*/
+/********************************************************************/
+/* Declaracion de constantes */
+declare @Int_Uno int
+
+/* Asignacion de valores a constantes */
+select  @Int_Uno = 1
+
+insert into SORIBGEN
+	(Rig_Numero,	Rig_NumRib,		Rig_ActCre,		Rig_TiDeGo,		Rig_DepGob,
+	Rig_TieExp,		Rig_Export,		Rig_TiGeDi,		Rig_GeCoMa,		Rig_GenCob,
+	Rig_GeNoCo,		Rig_Activo,		NumTransac,		Transaccio,		Usuario,
+	FechaSis,		SucOrigen,		SucDestino)
+	values (
+	@Rig_Numero,	@Rig_NumRib,	@Rig_ActCre,	@Rig_TiDeGo,	@Rig_DepGob,
+	@Rig_TieExp,	@Rig_Export,	@Rig_TiGeDi,	@Rig_GeCoMa,	@Rig_GenCob,
+	@Rig_GeNoCo,	@Rig_Activo,	@NumTransac,	@Transaccio,	@Usuario,
+	@FechaSis,		@SucOrigen,		@SucDestino)
+
+select @Rig_Numero = @@IDENTITY 
+
+if @@nestlevel = @Int_Uno begin 
+     select Err_Codigo = '000000', 
+			Err_Mensaj = 'Relacion agregada correctamente', 
+			Ric_Numero = @Rig_Numero 
+end
---
+++ SORIBGENMOD.sql
@@ -0,0 +1,65 @@
+create procedure SORIBGENMOD (
+   @Rig_Numero int,
+   @Rig_NumRib int,
+   @Rig_ActCre varchar(10),
+   @Rig_TiDeGo int,
+   @Rig_DepGob int,
+   @Rig_TieExp int,
+   @Rig_Export int,
+   @Rig_TiGeDi int,
+   @Rig_GeCoMa int,
+   @Rig_GenCob int,
+   @Rig_GeNoCo int,
+   @Rig_Activo bit,
+   
+   @NumTransac char(10),
+   @Transaccio char(3),
+   @Usuario char(6),
+   @FechaSis smalldatetime,
+   @SucOrigen char(3),
+   @SucDestino char(3),
+   @Modulo char(2)) 
+ as 
+/********************************************************************/
+/* DESCRIPCION: Modificacion de registros de Generalidades de RIB	*/
+/********************************************************************/
+/* Creo:		Raul Muniz											*/
+/* Fecha:		03/11/2020											*/
+/* Help:		1433413												*/
+/********************************************************************/
+/* Declaracion de Variables */
+
+if not exists (select Rig_Numero
+                   from SORIBGEN noholdlock
+                   where Rig_Numero = @Rig_Numero) begin
+   select  Err_Codigo = '000001',
+           Err_Mensaj	= 'El Numero de ID: ' +  convert(varchar,@Rig_Numero) + ' No Existe',
+           Err_Variab	= 'Rig_Numero'
+   rollback
+   return 1
+end
+
+update SORIBGEN set
+	Rig_Numero	= @Rig_Numero,
+	Rig_NumRib	= @Rig_NumRib,
+	Rig_ActCre	= @Rig_ActCre,
+	Rig_TiDeGo	= @Rig_TiDeGo,
+	Rig_DepGob	= @Rig_DepGob,
+	Rig_TieExp	= @Rig_TieExp,
+	Rig_Export	= @Rig_Export,
+	Rig_TiGeDi	= @Rig_TiGeDi,
+	Rig_GeCoMa	= @Rig_GeCoMa,
+	Rig_GenCob	= @Rig_GenCob,
+	Rig_GeNoCo	= @Rig_GeNoCo,
+	Rig_Activo	= @Rig_Activo,
+	NumTransac	= @NumTransac,
+	Transaccio	= @Transaccio,
+	Usuario		= @Usuario,
+	FechaSis	= @FechaSis,
+	SucOrigen	= @SucOrigen,
+	SucDestino	= @SucDestino
+where Rig_Numero = @Rig_Numero
+
+select Err_Codigo = '000000',
+       Err_Mensaj = 'Registro Modificado Correctamente',
+       Ric_Numero = @Rig_Numero
