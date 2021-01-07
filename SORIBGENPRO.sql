551a96d42ffac27dcaeaef7d97bbd8bf44df20b1
raul.muniz <raul.muniz@banregio.com>
2021-01-06 18:35:21 -0600

    se agregó tipo de proceso B

--- SORIBGENPRO.sql
+++ SORIBGENPRO.sql
@@ -21,17 +21,26 @@ as
 /****************************************************************/
 /* declaracion de constantes */
 declare	@Str_A		char(1),
+		@Str_B		char(1),
 		@Int_Cero	int,
 		@Int_Uno	int
 
 select	@Str_A		= 'A',
+		@Str_B		= 'B',
 		@Int_Cero	= 0,
 		@Int_Uno	= 1
 
 /* declaracion de variables */
 declare	@Int_RibBas	int,
 		@Str_NumPer	char(8),
-		@Int_NumSol int
+		@Int_NumSol int,
+		@Rig_TiDeGo	int,
+		@Rig_DepGob	int,
+		@Rig_TieExp	int,
+		@Rig_Export	int,
+		@Rig_PorExp	numeric(10,2),
+		@Rig_TiGeDi	int,
+		@Rig_GeCoMa	int
 
 if @Tip_Proces = @Str_A begin
 	
@@ -59,6 +68,36 @@ if @Tip_Proces = @Str_A begin
 					from SORIBGEN noholdlock
 					where Rig_NumRib = @Rig_NumRib
 		end
+	end
+end
+else if @Tip_Proces = @Str_B begin
+	
+	select @Str_NumPer = (select DISTINCT(Rib_NumPer) from SORIB noholdlock where Rib_Numero = @Rig_NumRib)
+	select @Int_NumSol = (select DISTINCT(Rib_NumSol) from SORIB noholdlock where Rib_Numero = @Rig_NumRib)
+	
+	if @Int_NumSol = @Int_Cero begin
+		select	@Rig_TiDeGo	= Rig_TiDeGo,	@Rig_DepGob = Rig_DepGob,	@Rig_TieExp = Rig_TieExp,	@Rig_Export = Rig_Export,	@Rig_PorExp = Rig_PorExp,
+				@Rig_TiGeDi = Rig_TiGeDi,	@Rig_GeCoMa = Rig_GeCoMa
+			from	SORIBGEN noholdlock
+			where	Rig_NumRib = @Rig_NumRib
+			
+		update SORIBGEN set
+			Rig_TiDeGo	= @Rig_TiDeGo,
+			Rig_DepGob	= @Rig_DepGob,
+			Rig_TieExp	= @Rig_TieExp,
+			Rig_Export	= @Rig_Export,
+			Rig_PorExp	= @Rig_PorExp,
+			Rig_TiGeDi	= @Rig_TiGeDi,
+			Rig_GeCoMa	= @Rig_GeCoMa,
+			NumTransac	= @NumTransac,
+			Transaccio	= @Transaccio,
+			Usuario		= @Usuario,
+			FechaSis	= @FechaSis,
+			SucOrigen	= @SucOrigen,
+			SucDestino	= @SucDestino
+		from 	SORIB noholdlock
+		where	Rib_Numero = Rig_NumRib
+		  and	Rib_NumPer = @Str_NumPer
 	end
 end
 
