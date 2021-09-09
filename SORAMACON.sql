994111b541dd6910e22ce95bd119da4c5e5d6f1a
Raul Muniz <raul.muniz@banregio.com>
2021-09-09 16:24:34 -0500

    SPs de consulta de actividad preponderante

---
+++ SORAMACON.sql
@@ -0,0 +1,51 @@
+create procedure SORAMACON (
+	@Ram_Numero int,
+	@Tip_Consul char(2),
+	@NumTransac	char(10),
+	@Transaccio	char(3),
+	@Usuario char(6),
+	@FechaSis smalldatetime,
+	@SucOrigen char(3),
+	@SucDestino	char(3),
+	@Modulo char(2)) 
+as
+
+/****************************************************************/
+/* DESCRIPCION: Consulta de registros de Rama					*/
+/****************************************************************/
+/** Creo:			Raul Muniz									*/
+/** Fecha:			09/09/2021                               	*/
+/** Help:			1504301					 					*/
+/****************************************************************/
+
+/* Declaracion de Variables */
+declare @Tip_ConTip	char(1),		/* Tipo consulta C/L */
+        @Tip_ConCon	char(1)			/* Numero consulta */
+
+/* Declaracion de Constantes */
+declare @Str_Uno	char(1),		/* Caracter 1 */
+		@Str_C		char(1),		/* Caracter C */
+		@Est_Activo	char(1)				/* Bit valor 1 */
+
+select @Str_C = 'C',
+       @Str_Uno = '1',
+	   @Est_Activo = 'A'
+
+select @Tip_ConTip = substring(@Tip_Consul, 1, 1),
+       @Tip_ConCon = substring(@Tip_Consul, 2, 1) 
+
+
+if @Tip_ConTip	= @Str_C begin /* 'C': Consulta */
+	if @Tip_ConCon = @Str_Uno begin		/* C1 */
+		select	Ram_Numero,	Ram_Descri,		Ram_Status,		Ram_Subsec,		NumTransac,
+				Transaccio,	Usuario,		FechaSis,		SucOrigen,		SucDestino
+		from SORAMA noholdlock
+		where	Ram_Numero	= @Ram_Numero
+	end
+end else begin
+	if @Tip_ConCon = @Str_Uno begin		/* L1 */
+		select	Ram_Numero,	Ram_Descri,		Ram_Status,		Ram_Subsec,		NumTransac,
+				Transaccio,	Usuario,		FechaSis,		SucOrigen,		SucDestino
+		from SORAMA noholdlock
+	end
+end
