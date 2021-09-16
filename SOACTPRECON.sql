66a69c742fe421f0ba1b14f5f0582c974a6d9e26
Raul Muniz <raul.muniz@banregio.com>
2021-09-15 20:50:11 -0500

    Agregar L2

--- SOACTPRECON.sql
+++ SOACTPRECON.sql
@@ -1,6 +1,8 @@
 create procedure SOACTPRECON (
 	@Acp_Numero int,
+	@Acp_Descri	varchar(254),
 	@Tip_Consul char(2),
+	
 	@NumTransac	char(10),
 	@Transaccio	char(3),
 	@Usuario char(6),
@@ -23,13 +25,17 @@ declare @Tip_ConTip	char(1),		/* Tipo consulta C/L */
         @Tip_ConCon	char(1)			/* Numero consulta */
 
 /* Declaracion de Constantes */
-declare @Str_Uno	char(1),		/* Caracter 1 */
-		@Str_C		char(1),		/* Caracter C */
-		@Est_Activo	char(1)				/* Bit valor 1 */
+declare @Str_C		char(1),		/* Caracter C */
+		@Str_Uno	char(1),		/* Caracter 1 */
+		@Str_Dos	char(1),		/* Caracter 2 */
+		@Est_Activo	char(1),		/* Estatus Activo */
+		@Str_Porcen	char(1)			/* String Porcentaje */
 
-select @Str_C = 'C',
-       @Str_Uno = '1',
-	   @Est_Activo = 'A'
+select @Str_C = 'C',				/* Caracter C */
+       @Str_Uno = '1',				/* Caracter 1 */
+       @Str_Dos = '2',				/* Caracter 2 */
+	   @Est_Activo = 'A',			/* Estatus Activo */
+	   @Str_Porcen	= '%'			/* String Porcentaje */
 
 select @Tip_ConTip = substring(@Tip_Consul, 1, 1),
        @Tip_ConCon = substring(@Tip_Consul, 2, 1) 
@@ -37,15 +43,34 @@ select @Tip_ConTip = substring(@Tip_Consul, 1, 1),
 
 if @Tip_ConTip	= @Str_C begin /* 'C': Consulta */
 	if @Tip_ConCon = @Str_Uno begin		/* C1 */
-		select	Acp_Numero,	Acp_Descri,		Acp_Status,		Acp_SubRam,		NumTransac,
-				Transaccio,	Usuario,		FechaSis,		SucOrigen,		SucDestino
-		from SOACTPRE noholdlock
-		where	Acp_Numero	= @Acp_Numero
+		select	Acp_Numero,	Acp_Descri,	Acp_Status,	Sur_Numero,	Sur_Descri,
+				Ram_Numero,	Ram_Descri,	Sus_Numero,	Sus_Descri,	Sec_Numero,
+				Sec_Descri
+			from SOACTPRE noholdlock
+			inner join	SOSUBRAM noholdlock
+				on Sur_Numero = Acp_SubRam
+			inner join	SORAMA noholdlock
+				on Ram_Numero = Sur_Rama
+			inner join	SOSUBSEC noholdlock
+				on Sus_Numero = Ram_Subsec
+			inner join	SOSECTOR noholdlock
+				on Sec_Numero = Sus_Sector
+			where	Acp_Numero	= @Acp_Numero
 	end
 end else begin
 	if @Tip_ConCon = @Str_Uno begin		/* L1 */
-		select	Acp_Numero,	Acp_Descri,		Acp_Status,		Acp_SubRam,		NumTransac,
-				Transaccio,	Usuario,		FechaSis,		SucOrigen,		SucDestino
-		from SOACTPRE noholdlock
+		select	Acp_Numero,	Acp_Descri,	Acp_Status,	Acp_SubRam,	NumTransac,
+				Transaccio,	Usuario,	FechaSis,	SucOrigen,	SucDestino
+			from SOACTPRE noholdlock
+			where	Acp_Status = @Est_Activo
+	end else if @Tip_ConCon = @Str_Dos begin		/* L2 */
+		select	@Acp_Descri = ltrim(rtrim(@Acp_Descri))
+		
+		select	Acp_Numero,	Acp_Descri,	Acp_Status,	Acp_SubRam,	NumTransac,
+				Transaccio,	Usuario,	FechaSis,	SucOrigen,	SucDestino
+			from SOACTPRE noholdlock
+			where	Acp_Status	= @Est_Activo
+			  and	Acp_Descri like @Str_Porcen + @Acp_Descri + @Str_Porcen
+			order by Acp_Descri
 	end
 end
