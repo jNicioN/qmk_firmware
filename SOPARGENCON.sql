20746d7c31d8ebe4d88f162d59867f5f8e286f9e
karla_morfin <karla.morfin@banregio.com>
2021-10-12 15:45:01 -0500

    Se agrega consulta de parámetro general por nombre.

--- SOPARGENCON.sql
+++ SOPARGENCON.sql
@@ -1,5 +1,6 @@
 create procedure SOPARGENCON (
 	@Par_Consec	int,
+	@Par_Nombre	varchar(50),
 	@Tip_Consul	char(2),
 
 	@NumTransac	char(10),
@@ -27,12 +28,14 @@ declare	@Tip_ConTip	char(1),	/* Declaración de variables */
 								
 declare	@Tip_ConInd	char(1),	/* Declaración de constantes */
 		@Tip_ConUno	char(1),
+		@Tip_ConDos	char(1),
 		@Ent_Uno	int,
 		@Ent_Dos	int
 
 /* Asignación de constantes */
 select	@Tip_ConInd	= 'C',		/* Tipo Consulta Individual*/
 		@Tip_ConUno	= '1',		/* Tipo Consulta Uno */
+		@Tip_ConDos	= '2',		/* Tipo Consulta Dos */
 		@Ent_Uno	= 1,		/* Entero Uno */
 		@Ent_Dos	= 2			/* Entero Dos */
 										
@@ -40,9 +43,19 @@ select	@Tip_ConTip	= substring(@Tip_Consul, @Ent_Uno, @Ent_Uno),
 		@Tip_ConCon	= substring(@Tip_Consul, @Ent_Dos, @Ent_Uno)
 		
 if @Tip_ConTip = @Tip_ConInd begin
+	
 	if @Tip_ConCon = @Tip_ConUno begin
+		
 		select	Par_Valor
 			from SOPARGEN noholdlock
 			where	Par_Consec	= @Par_Consec
+			
+	end else if @Tip_ConCon = @Tip_ConDos begin
+	
+		select	Par_Valor
+			from SOPARGEN noholdlock
+			where	Par_Nombre	= @Par_Nombre
+			
 	end
+	
 end
