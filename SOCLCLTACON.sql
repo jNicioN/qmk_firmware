a66cacef541ae2434a9592f2cc47106dbcff1c5d
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-04-30 09:35:32 -0500

    Se agrega consulta de clasificación por número de cuenta

--- SOCLCLTACON.sql
+++ SOCLCLTACON.sql
@@ -2,6 +2,7 @@ create procedure SOCLCLTACON (
 	@Cct_Clabe	char(18),
 	@Cct_Tarjet	char(16),
 	@Tip_Consul	char(2),
+	@Cue_Numero	char(12)
 
 	@NumTransac	char(10),
 	@Transaccio	char(3),
@@ -10,41 +11,50 @@ create procedure SOCLCLTACON (
 	@SucOrigen	char(3),
 	@SucDestino	char(3),
 	@Modulo		char(2))
+
 as
-/****************************************************************************/
-/* DESCRIPCION:   Consulta de clasificacion de producto por CLABE o tarjeta	*/
-/** REFERENCIAS:
+
+/***************************************************************************
+** DESCRIPCION:	Consulta de clasificación de producto por CLABE,		****
+** 				tarjeta o cuenta										****
+** REFERENCIAS:															****
+****************************************************************************
+** Modificó:	Seth Karim Luis Martínez								****
+** Fecha:		29/04/2022												****
+** Help:		1566195													****
+** Descripción: Consulta de clasificación por número de cuenta			****
 ****************************************************************************
 ** Creó:		Armida González											****
 ** Fecha:		02/03/2021												****
 ** Help:		1455332													****
 ***************************************************************************/
 
-/*	Declaracion de Variables	*/
+/*	Declaración de Variables	*/
 declare	@Tip_ConTip	char(1),
 		@Tip_ConCon	char(1),
 		@Cue_Tipo	char(2),
 		@Cue_Moneda	char(2),
 		@TaP_TipTar	char(4)
 	
-/*	Declaracion de Constantes	*/
+/*	Declaración de Constantes	*/
 declare	@Ent_Uno	int,
 		@Tip_TipCon	char(1),
 		@Tip_ConUno	char(1),
-		@Tip_ConDos	char(1)
+		@Tip_ConDos	char(1),
+		@Tip_ConTre char(1)
 
-/* Asignacion de Constantes */
+/* Asignación de Constantes */
 select	@Ent_Uno	= 1,			/* Entero Uno*/
 		@Tip_TipCon	= 'C',			/* Tipo consulta*/
-		@Tip_ConUno	= '1',			/* Uno - Consulta por producto de cheques*/
-		@Tip_ConDos	= '2'			/* Dos - Consulta por producto de tarjeta*/
+		@Tip_ConUno	= '1',			/* Uno - Consulta de clasificación por clabe */
+		@Tip_ConDos	= '2',			/* Dos - Consulta de clasificación por número de tarjeta */
+		@Tip_ConTre	= '3',			/* Tres - Consulta de clasificación por número de cuenta */
 
 select	@Tip_ConTip	= substring(@Tip_Consul, 1, 1),
 		@Tip_ConCon	= substring(@Tip_Consul, 2, 1)
 
-if @Tip_ConTip = @Tip_TipCon begin			/* Consulta */
-	if @Tip_ConCon = @Tip_ConUno begin				/* Consulta por cuenta CLABE*/
-
+if (@Tip_ConTip = @Tip_TipCon) begin			/* Consulta */
+	if (@Tip_ConCon = @Tip_ConUno) begin				/* Consulta por cuenta CLABE*/
 		select	@Cue_Tipo	= Cue_Tipo,
 				@Cue_Moneda	= Cue_Moneda
 			from CHCUENTA noholdlock
@@ -53,12 +63,10 @@ if @Tip_ConTip = @Tip_TipCon begin			/* Consulta */
 		select Clp_Numero, Clp_Clasif, Clp_Produc, Ptc_TipCue
 			from SOCLAPRO noholdlock,
 				 SOPRTICU noholdlock
-			where   Ptc_TipCue	= @Cue_Tipo
+			where	Ptc_TipCue	= @Cue_Tipo
 			  and	Ptc_Moneda	= @Cue_Moneda
-			  and	Clp_Produc	= Ptc_Produc	
-				
-	end if @Tip_ConCon = @Tip_ConDos begin			/* Consulta por número de tarjeta*/
-	
+			  and	Clp_Produc	= Ptc_Produc
+	end if (@Tip_ConCon = @Tip_ConDos) begin				/* Consulta por número de tarjeta*/
 		select	@TaP_TipTar	= TaP_TipTar
 			from CTTARPRO noholdlock
 			where	TaP_Tarjet	= @Cct_Tarjet
@@ -68,5 +76,11 @@ if @Tip_ConTip = @Tip_TipCon begin			/* Consulta */
 				 SOPRTITA noholdlock
 			where	Ptt_TipTar	= @TaP_TipTar
 			  and	Clp_Produc	= Ptt_Produc  
+	end if (@Tip_ConCon = @Tip_ConTre) begin				/* Consulta por número de cuenta*/
+		select	clp.Clp_Numero,	clp.Clp_Clasif,	clp.Clp_Produc,	ptc.Ptc_TipCue
+			from CHCUENTA cue
+			inner join SOPRTICU ptc on ptc.Ptc_TipCue = cue.Cue_Tipo and ptc.Ptc_Moneda = cue.Cue_Moneda
+			inner join SOCLAPRO clp on clp.Clp_Produc = ptc.Ptc_Produc
+			where	cue.Cue_Numero	= @Cue_Numero
 	end
 end
