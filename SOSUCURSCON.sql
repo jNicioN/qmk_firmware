e821c06311bb6291ca077c9ceccbeeed7655dfca
FRANCISCO JAVIER CARRILLO ROJAS <francisco.carrillo@banregio.com>
2022-09-21 10:41:00 -0500

    Incorporar referencia eliminada por error

--- SOSUCURSCON.sql
+++ SOSUCURSCON.sql
@@ -24,6 +24,11 @@ as
 ** HelpDesk:	1694392													****
 ** Descripcion:	Se modifica C1 para dar salida al campo  SoEstadoID 	****
 ****************************************************************************
+** Modifico:	Alan Santamaria											****
+** Fecha:		27/Julio/2021											****
+** HelpDesk:	1505299													****
+** Descripcion:	se agrega el campo de categoria en la consulta			****
+****************************************************************************
 ** Modifico:	Juan Pablo Mendez Cabrales								****
 ** Fecha:		06/Abr/2020												****
 ** HelpDesk:	01352603												****
