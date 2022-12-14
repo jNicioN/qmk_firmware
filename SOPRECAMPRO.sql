9c7e4b59b7894900f05f5b85dda4422a4ad98984
Shaila Palafox Hernández <58263860+bonjourshaila@users.noreply.github.com>
2022-12-14 16:17:32 -0600

    Validación de tipo de proceso

--- SOPRECAMPRO.sql
+++ SOPRECAMPRO.sql
@@ -18,7 +18,7 @@ create procedure SOPRECAMPRO (
 as
 
 /*******************************************************************
-** Descripcion : Guardar Precios de Cambio                         *
+** Descripcion : Proceso de Precios de Cambio                      *
 ********************************************************************
 ** REFERENCIAS:                       
 ********************************************************************
@@ -34,46 +34,50 @@ declare @Cam_Encont int,            /* Declaración de Variables */
 
 declare	@Ent_Cero   int,         	/* Declaración de Constantes */
         @Str_Vacio  int,
+        @Pro_Precio char(1),
         @Prc_Activo bit
 
 select  @Ent_Cero	=  0, 			/*	Entero Cero				 */
+        @Pro_Precio = 'P',          /*  Proceso de Precio        */
         @Prc_Activo =  1            /*  Status Ativo             */
-        
-select @Status      = @Ent_Cero
-select @FechaSis    = getdate()
-select @Dif_Fechas	= convert(int, datediff(dd, @Prc_Fecha, @FechaSis))
 
-select @Cam_Encont = count(*)
-    from SOPRECAM noholdlock
-    where Prc_Moneda   = @Prc_Moneda
-        and Prc_TipCam   = @Prc_TipCam
-        and Prc_TiOpCa   = @Prc_TiOpCa
+if @Tip_Proces = @Pro_Precio begin
+    select @Status      = @Ent_Cero
+    select @FechaSis    = getdate()
+    select @Dif_Fechas	= convert(int, datediff(dd, @Prc_Fecha, @FechaSis))
 
-if @Dif_Fechas = @Ent_Cero begin
-    if isnull(@Cam_Encont, @Ent_Cero) = @Ent_Cero begin
-        execute @Status = SOPRECAMALT
-            @Prc_Moneda,    @Prc_TipCam,    @Prc_TiOpCa,    @Prc_Precio,    @NumTransac,
-            @Transaccio,    @Usuario,       @FechaSis,      @SucOrigen,     @SucDestino,
-            @Modulo
-    end else begin
-        execute @Status = SOPRECAMACT
-            @Prc_Moneda,    @Prc_TipCam,    @Prc_TiOpCa,    @Prc_Precio,    @Prc_Activo,     @Tip_Proces,
-            @NumTransac,    @Transaccio,    @Usuario,       @FechaSis,      @SucOrigen,
-            @SucDestino,    @Modulo
+    select @Cam_Encont = count(*)
+        from SOPRECAM noholdlock
+        where Prc_Moneda   = @Prc_Moneda
+            and Prc_TipCam   = @Prc_TipCam
+            and Prc_TiOpCa   = @Prc_TiOpCa
+
+    if @Dif_Fechas = @Ent_Cero begin
+        if isnull(@Cam_Encont, @Ent_Cero) = @Ent_Cero begin
+            execute @Status = SOPRECAMALT
+                @Prc_Moneda,    @Prc_TipCam,    @Prc_TiOpCa,    @Prc_Precio,    @NumTransac,
+                @Transaccio,    @Usuario,       @FechaSis,      @SucOrigen,     @SucDestino,
+                @Modulo
+        end else begin
+            execute @Status = SOPRECAMACT
+                @Prc_Moneda,    @Prc_TipCam,    @Prc_TiOpCa,    @Prc_Precio,    @Prc_Activo,     @Tip_Proces,
+                @NumTransac,    @Transaccio,    @Usuario,       @FechaSis,      @SucOrigen,
+                @SucDestino,    @Modulo
+        end
+    end
+
+    if @Status = @Ent_Cero begin
+        select @Prc_Numero = Prc_Numero
+        from SOPRECAM noholdlock
+        where Prc_Moneda   = @Prc_Moneda
+            and Prc_TipCam   = @Prc_TipCam
+            and Prc_TiOpCa   = @Prc_TiOpCa
+
+        if isnull(@Prc_Numero, @Ent_Cero) <> @Ent_Cero begin
+            execute SODIPRCAPRO
+            @Prc_Numero,    @Prc_Fecha,     @Prc_Precio,    @NumTransac,    @Transaccio,
+            @Usuario,       @FechaSis,      @SucOrigen,     @SucDestino,    @Modulo
+        end
     end
 end
 
-if @Status = @Ent_Cero begin
-    select @Prc_Numero = Prc_Numero
-    from SOPRECAM noholdlock
-    where Prc_Moneda   = @Prc_Moneda
-        and Prc_TipCam   = @Prc_TipCam
-        and Prc_TiOpCa   = @Prc_TiOpCa
-
-    if isnull(@Prc_Numero, @Ent_Cero) <> @Ent_Cero begin
-        execute SODIPRCAPRO
-        @Prc_Numero,    @Prc_Fecha,     @Prc_Precio,    @NumTransac,    @Transaccio,
-        @Usuario,       @FechaSis,      @SucOrigen,     @SucDestino,    @Modulo
-    end
-end
-         
