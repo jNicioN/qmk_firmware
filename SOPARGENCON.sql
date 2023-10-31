894dc4f11e0297f11fe0af06957e1a5eeed3b299
ALBERTO FERNÁNDEZ CUELLAR <alberto.fernandez@hey.inc>
2023-10-31 11:32:20 -0600

    se resuelve bug nombre de columna no valido

--- SOPARGENCON.sql
+++ SOPARGENCON.sql
@@ -33,6 +33,7 @@ declare	@Tip_ConTip	char(1),	/* Declaración de variables */
 		@Url_Downlo varchar(200),
 		@Url_EndEma varchar(200),
 		@Url_ImaEma varchar(200)
+		
 								
 declare	@Tip_ConInd	char(1),	/* Declaración de constantes */
 		@Tip_ConUno	char(1),
@@ -51,7 +52,7 @@ declare @Par_EndPoi char(10), /* Declaración de variables Ec TDC*/
 select	@Tip_ConInd	= 'C',		/* Tipo Consulta Individual*/
 		@Tip_ConUno	= '1',		/* Tipo Consulta Uno */
 		@Tip_ConDos	= '2',		/* Tipo Consulta Dos */
-		@Tip_ConTres 	= '3',		/* Tipo Consulta Tres */
+		@Tip_ConTres = '3',		/* Tipo Consulta Tres */
 		@Ent_Uno	= 1,		/* Entero Uno */
 		@Ent_Dos	= 2,		/* Entero Dos */
 		@Ent_Tres	= 3,		/* Entero Tres */
@@ -76,7 +77,7 @@ if @Tip_ConTip = @Tip_ConInd begin
 		select	Par_Valor
 			from SOPARGEN noholdlock
 			where	Par_Nombre	= @Par_Nombre
-
+			
 	end else if @Tip_ConCon = @Tip_ConTres begin  	/* Consulta de propiedades de MS Ec TDC*/
 	
 			select @End_Point = Par_Valor 
@@ -99,8 +100,7 @@ if @Tip_ConTip = @Tip_ConInd begin
 				where Par_Nombre = @Par_UrlIma
 				order by Par_Consec asc
 				
-			select ltrim(rtrim(@End_Point)) Endpoint, rtrim(ltrim(@Url_Downlo)) Urldownload,rtrim(ltrim(@Url_EndEma))
-			
+			select ltrim(rtrim(@End_Point)) Endpoint, rtrim(ltrim(@Url_Downlo)) Urldownload,rtrim(ltrim(@Url_EndEma)) Urlemail, rtrim(ltrim(@Url_ImaEma)) Url_ImaEma
 	end
 	
 end
