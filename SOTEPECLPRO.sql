9ef2155623cfa28612bff4c1dcd2e6c625301e74
JOB JONATHAN MARTINEZ GONZALEZ <job.martinez@banregio.com>
2025-01-30 17:27:59 -0600

    se agrega nuevas validaciones

--- SOTEPECLPRO.sql
+++ SOTEPECLPRO.sql
@@ -1,5 +1,5 @@
 create procedure SOTEPECLPRO (
-    @Str_Telefo     char(10),
+    @Str_Telefo     char(20),
     @Per_Numero		char(8), 
 	@Cli_Numero		char(8),
 
@@ -30,6 +30,7 @@ declare @San_Telefo     bigint,
         @Status			int,
 	    @San_Lada		int
 
+
                     /*Declaracion de constantes*/
 declare	@Str_Vacio	    char(1),
         @Ent_Cero	    int,
@@ -52,29 +53,35 @@ select	@Str_Vacio	= '',
 
 if not isnull(@Str_Telefo, @Str_Vacio) = @Str_Vacio begin
     if (@Str_Telefo <> @Str_Guion) begin
-        select @San_Telefo = cast(right(RTrim(@Str_Telefo), @Ent_Diez) as bigint)
-        if @San_Telefo <> @Ent_Cero begin
-            
-            select @San_Lada = cast(left(@Str_Telefo,@Ent_Dos) as int)
-            select @ClClientID = ClClientID from CLCLIENT noholdlock where Cli_Numero = @Cli_Numero
-            select @PerPersoID = PerPersoID from SOPERSON noholdlock where Per_Numero = @Per_Numero
-            select @Val_Regist = @Ent_Uno from SOTELPER noholdlock where ClClientID = @ClClientID and PerPersoID = @PerPersoID and Tep_TipTel = @Cat_BOTs
-            
-            if isnull(@Val_Regist, @Ent_Cero) = @Ent_Cero begin
+        select @Str_Telefo = str_replace(@Str_Telefo,' ',null)
+        select @Str_Telefo = str_replace(@Str_Telefo,'-',null)
 
-                select @Status = @Ent_MenUno
+        if(patindex('%[A-Za-z!-/:-@[-`{-~]%',@Str_Telefo) = 0) begin
+            select @San_Telefo = cast(right(RTrim(@Str_Telefo), @Ent_Diez) as bigint)
 
-                exec @Status = SOTELPERALT @PerPersoID, @Cat_BOTs, @ClClientID, @San_Lada , @San_Telefo, 
-                                            @NumTransac ,@Transaccio ,@Usuario ,@FechaSis ,@SucOrigen ,
-                                            @SucDestino ,@Modulo
-            end
-            else begin
+            if @San_Telefo <> @Ent_Cero begin
 
-                select @Status = @Ent_MenUno
+                select @San_Lada = cast(left(@Str_Telefo,@Ent_Dos) as int)
+                select @ClClientID = ClClientID from CLCLIENT noholdlock where Cli_Numero = @Cli_Numero
+                select @PerPersoID = PerPersoID from SOPERSON noholdlock where Per_Numero = @Per_Numero
+                select @Val_Regist = @Ent_Uno from SOTELPER noholdlock where ClClientID = @ClClientID and PerPersoID = @PerPersoID and Tep_TipTel = @Cat_BOTs
 
-                exec @Status = SOTELPERMOD @PerPersoID, @Cat_BOTs, @ClClientID, @San_Lada, @San_Telefo, 
-                                            @NumTransac ,@Transaccio ,@Usuario ,@FechaSis ,@SucOrigen ,
-                                            @SucDestino ,@Modulo
+                if isnull(@Val_Regist, @Ent_Cero) = @Ent_Cero begin
+
+                    select @Status = @Ent_MenUno
+
+                    exec @Status = SOTELPERALT @PerPersoID, @Cat_BOTs, @ClClientID, @San_Lada , @San_Telefo, 
+                                                @NumTransac ,@Transaccio ,@Usuario ,@FechaSis ,@SucOrigen ,
+                                                @SucDestino ,@Modulo
+                end
+                else begin
+
+                    select @Status = @Ent_MenUno
+
+                    exec @Status = SOTELPERMOD @PerPersoID, @Cat_BOTs, @ClClientID, @San_Lada, @San_Telefo, 
+                                                @NumTransac ,@Transaccio ,@Usuario ,@FechaSis ,@SucOrigen ,
+                                                @SucDestino ,@Modulo
+                end
             end
         end
     end
