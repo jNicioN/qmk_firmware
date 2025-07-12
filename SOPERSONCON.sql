438caa87bc5cd66739537609fbe23f4b9daa6cec
Carlos Copto <carlos.copto@banregio.com>
2025-07-12 04:50:55 -0600

    Update SOPERSONCON.sql
    ajuste de consulta

--- SOPERSONCON.sql
+++ SOPERSONCON.sql
@@ -1471,40 +1471,55 @@ end else begin
 		end
 	end else if @Tip_ConCon = @Str_LetraB begin
 		
-		select 	case when NOM.Nol_Person is not null then NOM.Nol_Nombre else PER.Per_Nombre end as Per_Nombre,
+		select 
+			case when NOM.Nol_Person is not null then NOM.Nol_Nombre else PER.Per_Nombre end as Per_Nombre,
 			case when NOM.Nol_Person is not null then NOM.Nol_ApePat else PER.Per_ApePat end as Per_ApePat,
 			case when NOM.Nol_Person is not null then NOM.Nol_ApeMat else PER.Per_ApeMat end as Per_ApeMat,
 			PER.Per_RFC, PER.Per_Calle,
-			PER.Per_CalNum,	PER.Per_Coloni,	PER.Per_Locali,	PER.Per_CodPos,	PER.Per_Telefo,
-			PER.Per_EstCiv,	PER.Per_Nacion,	PER.Per_ActEmp,	PER.Per_Activi,
-			Per_FecNac = PDI.Adi_FecNac, PDI.Adi_FecNac,
-			PER.PerPersoID, PER.Per_Tipo, PDI.Adi_Sexo,	PER.Per_CURP, PER.Per_Comple,
-			PER.Per_Entida, PER.Per_LadTel, PDI.Adi_FecCon, PER.Per_ActINE, PDI.Adi_FecCon,
-			case when NOM.Nol_Person is not null then NOM.Nol_RazSoc else PER.Per_RazSoc end as Per_RazSoc, PER.Per_Numero,
-			PER.Per_NumTra, PER.Per_NuSeFi, PER.Per_Titulo, PER.Per_ApaPos, PER.Per_Email,
-			PER.Per_ComDom, PER.Per_Nacion, PER.Per_Giro, PER.Per_Sector,
+			PER.Per_CalNum, PER.Per_Coloni, PER.Per_Locali, PER.Per_CodPos,
+			PER.Per_Telefo, PER.Per_EstCiv, PER.Per_Nacion, PER.Per_ActEmp, PER.Per_Activi,
+			PER.Per_Numero, PER.Per_Fecha, PER.Per_NumTra, PER.Per_Tipo, PER.Per_NuSeFi,
+			PER.Per_Titulo, PER.Per_CURP,  PER.Per_Entida, PER.Per_ApaPos,
+			PER.Per_LadTel, PER.Per_Email, PER.Per_ComDom, PER.Per_Giro, PER.Per_Sector,
+			PER.Per_Comple, PER.Per_ActINE, PER.PerPersoID, Per_FecNac = PDI.Adi_FecNac,
+			
+			PDI.Adi_PerNum, PDI.Adi_Fecha, PDI.Adi_NumTra, PDI.Adi_LugNac, PDI.Adi_Sexo,
+			PDI.Adi_FecNac, PDI.Adi_RegMat, PDI.Adi_VivCas, PDI.Adi_TieRes, PDI.Adi_Fax,
+			PDI.Adi_NumDep, PDI.Adi_Puesto, PDI.Adi_Ocupac, PDI.Adi_AntLab, PDI.Adi_LugTra,
+			PDI.Adi_TelTra, PDI.Adi_CalTra, PDI.Adi_NuCaTr, PDI.Adi_ColTra, PDI.Adi_Locali,
+			PDI.Adi_CPTra, PDI.Adi_FecCon, PDI.Adi_CaNuIn, PDI.Adi_NacExt, PDI.Adi_Reside,
+			PDI.Adi_DocEst, PDI.Adi_OtDoEs, PDI.Adi_FeExDo, PDI.Adi_CalInm, PDI.Adi_CalExt,
+			PDI.Adi_CaNuEx, PDI.Adi_ColExt, PDI.Adi_LocExt, PDI.Adi_EntExt, PDI.Adi_PaiExt,
+			PDI.Adi_CoPoEx, PDI.Adi_TelExt, PDI.Adi_TipIde, PDI.Adi_OtrIde, PDI.Adi_NumIde,
+			PDI.Adi_FeExId, PDI.Adi_FeVeId, PDI.Adi_NuIdFi, PDI.Adi_EntPri, PDI.Adi_EntSeg,
+			case when NOM.Nol_Person is not null then NOM.Nol_RazSoc else PER.Per_RazSoc end as Per_RazSoc,
 			max(isnull(CAD.Adi_Client,'')) as Adi_Client
 		from SOPERSON PER noholdlock
-		left join SOPERADI PDI noholdlock on PER.Per_Numero	= PDI.Adi_PerNum
+		left join SOPERADI PDI noholdlock on PER.Per_Numero = PDI.Adi_PerNum
 		left join CLADICIO CAD noholdlock on PER.Per_Numero = CAD.Adi_NumPer
 		left join SONOMLAR NOM noholdlock on PER.PerPersoID = NOM.Nol_Person
-		where	PER.Per_RFC	= @Per_RFC
+		where PER.Per_RFC = @Per_RFC
 		group by 
 			case when NOM.Nol_Person is not null then NOM.Nol_Nombre else PER.Per_Nombre end,
 			case when NOM.Nol_Person is not null then NOM.Nol_ApePat else PER.Per_ApePat end,
 			case when NOM.Nol_Person is not null then NOM.Nol_ApeMat else PER.Per_ApeMat end,
 			PER.Per_RFC, PER.Per_Calle,
-			PER.Per_CalNum,	PER.Per_Coloni,	PER.Per_Locali,	PER.Per_CodPos,	PER.Per_Telefo,
-			PER.Per_EstCiv,	PER.Per_Nacion,	PER.Per_ActEmp,	PER.Per_Activi,
-			PDI.Adi_FecNac,
-			PER.PerPersoID, PER.Per_Tipo, PDI.Adi_Sexo,	PER.Per_CURP, PER.Per_Comple,
-			PER.Per_Entida, PER.Per_LadTel, PDI.Adi_FecCon, PER.Per_ActINE, PDI.Adi_FecCon,
-			case when NOM.Nol_Person is not null then NOM.Nol_RazSoc else PER.Per_RazSoc end,
-			PER.Per_Numero,
-			PER.Per_NumTra, PER.Per_NuSeFi, PER.Per_Titulo, PER.Per_ApaPos, PER.Per_Email,
-			PER.Per_ComDom, PER.Per_Giro, PER.Per_Sector
+			PER.Per_CalNum, PER.Per_Coloni, PER.Per_Locali, PER.Per_CodPos,
+			PER.Per_Telefo, PER.Per_EstCiv, PER.Per_Nacion, PER.Per_ActEmp, PER.Per_Activi,
+			PER.Per_Numero, PER.Per_Fecha, PER.Per_NumTra, PER.Per_Tipo, PER.Per_NuSeFi,
+			PER.Per_Titulo, PER.Per_CURP,  PER.Per_Entida, PER.Per_ApaPos,
+			PER.Per_LadTel, PER.Per_Email, PER.Per_ComDom, PER.Per_Giro, PER.Per_Sector,
+			PER.Per_Comple, PER.Per_ActINE, PER.PerPersoID,
+			PDI.Adi_PerNum, PDI.Adi_Fecha, PDI.Adi_NumTra, PDI.Adi_LugNac, PDI.Adi_Sexo,
+			PDI.Adi_FecNac, PDI.Adi_RegMat, PDI.Adi_VivCas, PDI.Adi_TieRes, PDI.Adi_Fax,
+			PDI.Adi_NumDep, PDI.Adi_Puesto, PDI.Adi_Ocupac, PDI.Adi_AntLab, PDI.Adi_LugTra,
+			PDI.Adi_TelTra, PDI.Adi_CalTra, PDI.Adi_NuCaTr, PDI.Adi_ColTra, PDI.Adi_Locali,
+			PDI.Adi_CPTra, PDI.Adi_FecCon, PDI.Adi_CaNuIn, PDI.Adi_NacExt, PDI.Adi_Reside,
+			PDI.Adi_DocEst, PDI.Adi_OtDoEs, PDI.Adi_FeExDo, PDI.Adi_CalInm, PDI.Adi_CalExt,
+			PDI.Adi_CaNuEx, PDI.Adi_ColExt, PDI.Adi_LocExt, PDI.Adi_EntExt, PDI.Adi_PaiExt,
+			PDI.Adi_CoPoEx, PDI.Adi_TelExt, PDI.Adi_TipIde, PDI.Adi_OtrIde, PDI.Adi_NumIde,
+			PDI.Adi_FeExId, PDI.Adi_FeVeId, PDI.Adi_NuIdFi, PDI.Adi_EntPri, PDI.Adi_EntSeg,
+			case when NOM.Nol_Person is not null then NOM.Nol_RazSoc else PER.Per_RazSoc end
 
 	end
-	
-	
 end
