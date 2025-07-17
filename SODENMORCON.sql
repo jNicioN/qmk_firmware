09f5aec8604a0088fbe8be6583757eef6883ae6f
Diego Armando Valdes Reyna <diego.valdes@banregio.com>
2025-07-17 13:34:42 -0600

    se corrige sp SODENMORCON por estandares

--- SODENMORCON.sql
+++ SODENMORCON.sql
@@ -1,111 +1,103 @@
 create procedure SODENMORCON (
-    @Dem_Moneda     varchar(3),
-    @Dem_Denomi     money,
-    @Tip_Consul     varchar(2),
+    @Dem_Moneda	char(2), 
+    @Dem_Denomi money, 
+    @Tip_Consul	char(2),
 
-    @NumTransac     char(10),
-    @Transaccio     char(3),
-    @Usuario        char(6),
-    @FechaSis       smalldatetime,
-    @SucOrigen      char(3),
-    @SucDestino     char(3),
-    @Modulo         char(2)
+    @NumTransac	char(10),
+    @Transaccio	char(3),
+    @Usuario	char(6),
+    @FechaSis	smalldatetime,
+    @SucOrigen	char(3),
+    @SucDestino	char(3),
+    @Modulo		char(2)
 )
 as
 
 /*************************************************************************
 *   DESCRIPCION: Consulta registros de la tabla SODENMOR según filtros   *
-*************************************************************************/
+**************************************************************************
+* REFERENCIAS:
+**************************************************************************
+** Creó:       Diego Valdés                                            ***
+** Fecha:      25/06/2025                                              ***
+** Descripción: Procedimiento para consulta de monedas y denominaciones**
+************************************************************************/
 
-declare @TIPO_L1              varchar(3),
-        @TIPO_C1              varchar(3),
-        @TIPO_C2              varchar(3),
-        @STATUS_OK            int,
-        @STATUS_BAD_REQUEST   int,
-        @STATUS_NOT_FOUND     int,
-        @STATUS_ERROR         int,
-        @MSG_NOT_FOUND        varchar(50),
-        @MSG_BAD_REQUEST      varchar(100),
-        @MSG_BAD_REQUEST_C1   varchar(100),
-        @MSG_BAD_REQUEST_C2   varchar(100),
-        @MSG_INVALID_CHARS    varchar(100),
-        @MSG_INTERNAL_ERROR   varchar(50)
+-- Declaración de constantes
+declare @Str_Lista   char(1),
+        @Str_Consul  char(1),
+        @Str_Uno     char(1),
+        @Str_Dos     char(1),
+        @Str_Vacio   char(1),
+        @Tip_ConTip  char(1),
+        @Tip_ConCon  char(1),
+        @Str_Permit  char(12),
+        @Str_Porcen  char(1)
 
-select
-    @TIPO_L1            = 'L1',
-    @TIPO_C1            = 'C1',
-    @TIPO_C2            = 'C2',
-    @STATUS_OK          = 200,
-    @STATUS_BAD_REQUEST = 400,
-    @STATUS_NOT_FOUND   = 404,
-    @STATUS_ERROR       = 500,
-    @MSG_NOT_FOUND      = 'CODE_NOT_FOUND',
-    @MSG_BAD_REQUEST    = 'CODE_BAD_REQUEST',
-    @MSG_BAD_REQUEST_C1 = 'CODE_BAD_REQUEST - moneda requerida para tipo C1',
-    @MSG_BAD_REQUEST_C2 = 'CODE_BAD_REQUEST - moneda y denominacion requeridas para tipo C2',
-    @MSG_INVALID_CHARS  = 'CODE_BAD_REQUEST - parámetros contienen caracteres inválidos',
-    @MSG_INTERNAL_ERROR = 'CODE_INTERNAL_ERROR'
+select  @Str_Lista   = 'L',
+        @Str_Consul  = 'C',
+        @Str_Uno     = '1',
+        @Str_Dos     = '2',
+        @Str_Vacio   = '',
+        @Tip_ConTip  = substring(@Tip_Consul, 1, 1),
+        @Tip_ConCon  = substring(@Tip_Consul, 2, 1),
+        @Str_Permit  = '[^a-zA-Z0-9]', 
+        @Str_Porcen  = '%'
 
-/* Validación de tipoConsulta */
-if @Tip_Consul not in (@TIPO_L1, @TIPO_C1, @TIPO_C2)
-begin
-    select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST as mensaje
-    return
-end
+select  @Dem_Moneda = isnull(@Dem_Moneda, @Str_Vacio),
+        @Dem_Denomi = isnull(@Dem_Denomi, 0)
 
-/* Validación de parámetros requeridos */
-if @Tip_Consul = @TIPO_C1 and isnull(@Dem_Moneda, '') = ''
-begin
-    select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST_C1 as mensaje
-    return
-end
+if @Tip_ConTip = @Str_Consul begin  -- CONSULTAS
 
-if @Tip_Consul = @TIPO_C2 and (isnull(@Dem_Moneda, '') = '' or @Dem_Denomi is null)
-begin
-    select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST_C2 as mensaje
-    return
-end
+    if @Dem_Moneda = @Str_Vacio begin 
+        select	Err_Codigo = '00001',
+                Err_Mensaj = 'La moneda no puede ser vacía',
+                Err_Variab = 'Dem_Moneda'
+        return 1
+    end 
 
-/* Validación de caracteres inválidos solo para cadenas */
-if (@Dem_Moneda <> '' and @Dem_Moneda like '%[^a-zA-Z0-9]%')
-   or (@Tip_Consul like '%[^a-zA-Z0-9]%')
-begin
-    select @STATUS_BAD_REQUEST as status_code, @MSG_INVALID_CHARS as mensaje
-    return
-end
-
-/* Ejecuciones */
-if @Tip_Consul = @TIPO_L1
-begin
-    if exists (select 1 from SODENMOR)
+    -- Validación de caracteres no permitidos (solo en campos tipo char)
+    if (@Dem_Moneda like @Str_Porcen + @Str_Permit + @Str_Porcen)
+        or (@Tip_Consul like @Str_Porcen + @Str_Permit + @Str_Porcen)
     begin
-        select * from SODENMOR
+        select	Err_Codigo = '00002',
+                Err_Mensaj = 'No se permiten caracteres especiales',
+                Err_Variab = 'Dem_Moneda, Tip_Consul'
+        return 1
     end
-    else
-    begin
-        select @STATUS_NOT_FOUND as status_code, @MSG_NOT_FOUND as mensaje
-    end
-end
-else if @Tip_Consul = @TIPO_C1
-begin
-    if exists (select 1 from SODENMOR where Dem_Moneda = @Dem_Moneda)
-    begin
-        select * from SODENMOR where Dem_Moneda = @Dem_Moneda
-    end
-    else
-    begin
-        select @STATUS_NOT_FOUND as status_code, @MSG_NOT_FOUND as mensaje
-    end
-end
-else if @Tip_Consul = @TIPO_C2
-begin
-    if exists (select 1 from SODENMOR where Dem_Moneda = @Dem_Moneda and Dem_Denomi = @Dem_Denomi)
-    begin
-        select * from SODENMOR where Dem_Moneda = @Dem_Moneda and Dem_Denomi = @Dem_Denomi
-    end
-    else
-    begin
-        select @STATUS_NOT_FOUND as status_code, @MSG_NOT_FOUND as mensaje
-    end
-end
 
+    if @Tip_ConCon = @Str_Dos begin  -- Consulta por Llave Principal
+
+        if @Dem_Denomi = 0 begin 
+            select	Err_Codigo = '00003',
+                    Err_Mensaj = 'La denominación no puede ser cero',
+                    Err_Variab = 'Dem_Denomi'
+            return 1
+        end 
+
+        if @Dem_Denomi < 0 begin 
+            select	Err_Codigo = '00004',
+                    Err_Mensaj = 'La denominación no puede ser negativa',
+                    Err_Variab = 'Dem_Denomi'
+            return 1
+        end
+
+        select	Dem_Moneda, Dem_Denomi, Dem_Descri
+        from SODENMOR noholdlock
+        where Dem_Moneda = @Dem_Moneda
+          and Dem_Denomi = @Dem_Denomi
+
+    end 
+    else if @Tip_ConCon = @Str_Uno begin  -- Consulta por Moneda
+        select	Dem_Moneda, Dem_Denomi, Dem_Descri
+        from SODENMOR noholdlock
+        where Dem_Moneda = @Dem_Moneda
+    end 
+
+end  -- cierre de if @Tip_ConTip = @Str_Consul
+
+-- Listado completo (tipo L1)
+if @Tip_ConTip = @Str_Lista and @Tip_ConCon = @Str_Uno begin
+    select	Dem_Moneda, Dem_Denomi, Dem_Descri
+    from SODENMOR noholdlock
+end 
