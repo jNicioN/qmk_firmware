7fefd3311497b0699ef60108812679ce490e9735
Diego Armando Valdes Reyna <diego.valdes@banregio.com>
2025-07-17 13:25:12 -0600

    se corrige sp SODENMORCON por estandares

--- SODENMORCON.sql
+++ SODENMORCON.sql
@@ -1,29 +1,21 @@
-
 create procedure SODENMORCON (
-    @moneda        varchar(10),
-    @denominacion  varchar(20),
-    @tipoConsulta  varchar(3),
+    @Dem_Moneda     varchar(3),
+    @Dem_Denomi     money,
+    @Tip_Consul     varchar(2),
 
-    @NumTransac    char(10),
-    @Transaccio    char(3),
-    @Usuario       char(6),
-    @FechaSis      smalldatetime,
-    @SucOrigen     char(3),
-    @SucDestino    char(3),
-    @Modulo        char(2)
+    @NumTransac     char(10),
+    @Transaccio     char(3),
+    @Usuario        char(6),
+    @FechaSis       smalldatetime,
+    @SucOrigen      char(3),
+    @SucDestino     char(3),
+    @Modulo         char(2)
 )
 as
 
 /*************************************************************************
 *   DESCRIPCION: Consulta registros de la tabla SODENMOR según filtros   *
-**************************************************************************
-* REFERENCIAS:
-**************************************************************************
-** Creó:       Diego Valdés                                            ***
-** Fecha:      25/06/2025                                              ***
-** Descripción: Procedimiento para consulta de monedas y denominaciones**
-************************************************************************/
-
+*************************************************************************/
 
 declare @TIPO_L1              varchar(3),
         @TIPO_C1              varchar(3),
@@ -54,103 +46,62 @@ select
     @MSG_INVALID_CHARS  = 'CODE_BAD_REQUEST - parámetros contienen caracteres inválidos',
     @MSG_INTERNAL_ERROR = 'CODE_INTERNAL_ERROR'
 
-/* Validación de tipoConsulta: solo se permiten L1, C1 y C2 */
-if @tipoConsulta not in (@TIPO_L1, @TIPO_C1, @TIPO_C2)
+/* Validación de tipoConsulta */
+if @Tip_Consul not in (@TIPO_L1, @TIPO_C1, @TIPO_C2)
 begin
     select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST as mensaje
     return
 end
 
-/* Validación de parámetros obligatorios según tipoConsulta */
-if @tipoConsulta = @TIPO_C1 and isnull(@moneda, '') = ''
+/* Validación de parámetros requeridos */
+if @Tip_Consul = @TIPO_C1 and isnull(@Dem_Moneda, '') = ''
 begin
     select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST_C1 as mensaje
     return
 end
 
-if @tipoConsulta = @TIPO_C2 and (isnull(@moneda, '') = '' or isnull(@denominacion, '') = '')
+if @Tip_Consul = @TIPO_C2 and (isnull(@Dem_Moneda, '') = '' or @Dem_Denomi is null)
 begin
     select @STATUS_BAD_REQUEST as status_code, @MSG_BAD_REQUEST_C2 as mensaje
     return
 end
 
-/* Validación de caracteres no permitidos en los parámetros */
-if (@moneda <> '' and @moneda like '%[^a-zA-Z0-9]%')
-   or (@denominacion <> '' and @denominacion like '%[^a-zA-Z0-9]%')
-   or (@tipoConsulta like '%[^a-zA-Z0-9]%')
+/* Validación de caracteres inválidos solo para cadenas */
+if (@Dem_Moneda <> '' and @Dem_Moneda like '%[^a-zA-Z0-9]%')
+   or (@Tip_Consul like '%[^a-zA-Z0-9]%')
 begin
     select @STATUS_BAD_REQUEST as status_code, @MSG_INVALID_CHARS as mensaje
     return
 end
 
-/* Validación adicional: denominación debe ser numérica si es tipo C2 */
-if @tipoConsulta = @TIPO_C2 and isnumeric(@denominacion) = 0
-begin
-    select @STATUS_BAD_REQUEST as status_code,
-           'CODE_BAD_REQUEST - denominacion debe ser numérica' as mensaje
-    return
-end
-
-/* Ejecución según el tipo de consulta */
-if @tipoConsulta = @TIPO_L1
+/* Ejecuciones */
+if @Tip_Consul = @TIPO_L1
 begin
     if exists (select 1 from SODENMOR)
     begin
-        select
-            Dem_Moneda,
-            Dem_Denomi,
-            Dem_Descri,
-            NumTransac,
-            Transaccio,
-            Usuario,
-            FechaSis,
-            SucOrigen,
-            SucDestino
-        from SODENMOR
+        select * from SODENMOR
     end
     else
     begin
         select @STATUS_NOT_FOUND as status_code, @MSG_NOT_FOUND as mensaje
     end
 end
-else if @tipoConsulta = @TIPO_C1
+else if @Tip_Consul = @TIPO_C1
 begin
-    if exists (select 1 from SODENMOR where Dem_Moneda = @moneda)
+    if exists (select 1 from SODENMOR where Dem_Moneda = @Dem_Moneda)
     begin
-        select
-            Dem_Moneda,
-            Dem_Denomi,
-            Dem_Descri,
-            NumTransac,
-            Transaccio,
-            Usuario,
-            FechaSis,
-            SucOrigen,
-            SucDestino
-        from SODENMOR
-        where Dem_Moneda = @moneda
+        select * from SODENMOR where Dem_Moneda = @Dem_Moneda
     end
     else
     begin
         select @STATUS_NOT_FOUND as status_code, @MSG_NOT_FOUND as mensaje
     end
 end
-else if @tipoConsulta = @TIPO_C2
+else if @Tip_Consul = @TIPO_C2
 begin
-    if exists (select 1 from SODENMOR where Dem_Moneda = @moneda and Dem_Denomi = convert(money, @denominacion))
+    if exists (select 1 from SODENMOR where Dem_Moneda = @Dem_Moneda and Dem_Denomi = @Dem_Denomi)
     begin
-        select
-            Dem_Moneda,
-            Dem_Denomi,
-            Dem_Descri,
-            NumTransac,
-            Transaccio,
-            Usuario,
-            FechaSis,
-            SucOrigen,
-            SucDestino
-        from SODENMOR
-        where Dem_Moneda = @moneda and Dem_Denomi = convert(money, @denominacion)
+        select * from SODENMOR where Dem_Moneda = @Dem_Moneda and Dem_Denomi = @Dem_Denomi
     end
     else
     begin
@@ -158,11 +109,3 @@ begin
     end
 end
 
-/* Confirmación de ejecución exitosa si no hubo errores y nivel base */
-if @@nestlevel = 1
-begin
-    select
-        Err_Numero = '00000',
-        Err_Mensaj = '',
-        Consulta_Ejecutada = @tipoConsulta
-end
