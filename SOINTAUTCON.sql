e672e7f5df755c0e105f07b83efc0552c0936936
Luis Daniel Nevárez Pérez <luis.nevarez@banregio.com>
2025-09-24 11:07:44 -0600

    Cambios para FM para PM

--- SOINTAUTCON.sql
+++ SOINTAUTCON.sql
@@ -21,6 +21,12 @@ as
 ** DESCRIPCION: Consulta de intervinientes Autorizados			  **
 **				(para Identificación de Operaciones)			  **
 ********************************************************************
+** Modificó:	Daniel Nevarez									****
+** Fecha:		17/09/2025										****
+** Help:		TCELES-36275									****
+** Descripcion:	Incluir consulta a CHCOTBEN para PM en			****
+				modulo de CLI									****
+********************************************************************
 ** Modificó:	Francisco Javier Carrillo Rojas					****
 ** Fecha:		07/Abril/2020									****
 ** Help:		01379522										****
@@ -66,7 +72,8 @@ declare	@Tip_ConTip	char(1),	/* Consulta Tipo C/L*/
 		@Cio_Status	char(1),	/* Estatus de configuración */
 		@Ins_PerCli	int,		/* Bandera para saber si se inserta el registro de la persona del cliente */
 		@Fof_MaxCon	smallint,	/* Consecutivo máximo del registro de firmas */
-		@Cio_IntReq	char(2)		/* Intervinientes requeridos */
+		@Cio_IntReq	char(2),	/* Intervinientes requeridos */
+		@Cli_Tipo	char(1)		/* Tipo de cliente (física o moral) */
 
 
 /* Declaracion de Constantes */
@@ -77,6 +84,7 @@ declare	@Str_C		char(1),
 		@Str_Vacio	char(1),
 		@Ent_Cero	int,
 		@Tip_PerFis	char(1),
+		@Tip_PerMor	char(1),
 		@Tip_Titula	char(1),
 		@Tip_Cotitu	char(1),
 		@Tip_Tercer	char(1),
@@ -103,6 +111,7 @@ select	@Str_C		= 'C',		/* Tipo C */
 		@Str_Vacio	= '',		/* String vacío */
 		@Ent_Cero	= 0,		/* Entero en cero */
 		@Tip_PerFis	= '2',		/* Tipo de persona persona física */
+		@Tip_PerMor	= '1',		/* Tipo de persona persona moral */
 		@Tip_Titula	= '1',		/* Tipo de interviniente Titular */
 		@Tip_Cotitu	= '3',		/* Tipo de interviniente Cotitular */
 		@Tip_Tercer	= '7',		/* Tipo de interviniente Tercero autorizado */
@@ -162,17 +171,36 @@ select	@Ins_PerCli	= @Ent_Cero
 /*Base para poder obtener los intervinientes autorizados de acuerdo a la configuración dada */
 if isnull(@Opi_TipOpe, @Ent_Cero) != @Ent_Cero and isnull(@Cio_Status, @Str_Vacio) = @Sta_Activo begin
 	if @Opi_BasOpe	= @Tip_BasCli begin
+
+		select	@Cli_Tipo	= Cli_Tipo
+			 from	CLCLIENT noholdlock
+			where	Cli_Numero	= @Cli_Numero
+
 		/* Solo podemos tener el número de cliente */
-		insert into #PersonasAutorizadas
-			select	adi.Adi_NumPer,	@Tip_Titula, @Ent_Cero
-				from CLCLIENT cli noholdlock
-					 inner join CLADICIO adi noholdlock on adi.ClClientID = cli.ClClientID 					
-				where	cli.Cli_Numero	= @Cli_Numero
-				  and	cli.Cli_Tipo	= @Tip_PerFis
-				  and	@Tip_Titula in(select Tii_TipInt
-										from SOTIINID noholdlock
-										where	Tii_NuIdCo	= @Cio_NuIdCo
-										  and	Tii_Status	= @Sta_Activo)
+		if @Cli_Tipo = @Tip_PerFis begin 
+
+			insert into #PersonasAutorizadas
+				select	adi.Adi_NumPer,	@Tip_Titula, @Ent_Cero
+					from CLCLIENT cli noholdlock
+						inner join CLADICIO adi noholdlock on adi.ClClientID = cli.ClClientID 					
+					where	cli.Cli_Numero	= @Cli_Numero
+					and	cli.Cli_Tipo	= @Tip_PerFis
+					and	@Tip_Titula in(select Tii_TipInt
+											from SOTIINID noholdlock
+											where	Tii_NuIdCo	= @Cio_NuIdCo
+											  and	Tii_Status	= @Sta_Activo)
+		end else if @Cli_Tipo = @Tip_PerMor begin
+
+			insert into #PersonasAutorizadas
+				select	Cob_Person,	Cob_Tipo, @Ent_Cero
+					from CHCOTBEN noholdlock
+					where	Cob_Cuenta	like (@Cli_Numero + '%')
+					  and	Cob_Tipo in( select Tii_TipInt
+											from SOTIINID noholdlock
+											where	Tii_NuIdCo	= @Cio_NuIdCo
+											  and	Tii_Status	= @Sta_Activo)
+
+		end
 				
 	end else if @Opi_BasOpe	= @Tip_BasCue begin
 		/* Se obtienen por referencias en cotitulares/beneficiarios/etc */
